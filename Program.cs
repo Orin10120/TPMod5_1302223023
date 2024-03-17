18cf5bae6696f7c6197334cd3e1751bc5ebc5636
Orin10120 <rindang3001@gmail.com>
2024-03-17 23:35:44 +0700

    Hallo Generic

--- Program.cs
+++ Program.cs
@@ -26,34 +26,5 @@ namespace TPModul5_1302223023
             Console.WriteLine("Halo User " + input);
         }
     }
-    public class DataGeneric<T>
-    {
-        private T x;
-
-        public DataGeneric(T x) { 
-           this.x = x;
-        }
-
-        public void printData()
-        {
-            Console.WriteLine("Data yang tersimpan adalah: " + x);
-        }
-    }
-     class Program
-    {
-        
-        static void Main(string[] args)
-        {
-            String MyNim = "1302223023";
-            String MyName = "Rindang";
-
-            DataGeneric<String> dataNew = new DataGeneric<string>(MyNim);
-
-            SapaUser<String> user = new SapaUser<string>(MyName);
-
-            user.PrintUser();
-            dataNew.printData();
-           
-        }
-    }
+   
 }
