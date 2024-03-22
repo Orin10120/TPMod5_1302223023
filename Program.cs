dd0cab41536713376f112dada7459d32564472d1
Orin10120 <rindang3001@gmail.com>
2024-03-23 05:53:41 +0700

    finish TP

--- Program.cs
+++ Program.cs
@@ -30,14 +30,30 @@ namespace TPModul5_1302223023
     {
         private T x;
 
-        public DataGeneric(T x)
-        {
-            this.x = x;
+        public DataGeneric(T x) { 
+           this.x = x;
         }
 
         public void printData()
         {
             Console.WriteLine("Data yang tersimpan adalah: " + x);
+        }
+    }
+     class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            String MyNim = "1302223023";
+            String MyName = "Rindang";
+
+            DataGeneric<String> dataNew = new DataGeneric<string>(MyNim);
+
+            SapaUser<String> user = new SapaUser<string>(MyName);
+
+            user.PrintUser();
+            dataNew.printData();
+           
         }
     }
 }
